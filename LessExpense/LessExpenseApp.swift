c92d070f867850099bd4978c2fe8fa162e0e36f7
Nik Galkin <53794828+thegalkin@users.noreply.github.com>
2020-11-10 23:16:09 +0300

    Initial Commit

---
+++ LessExpense/LessExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  LessExpenseApp.swift
+//  LessExpense
+//
+//  Created by Никита Галкин on 10.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct LessExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
